ea3b1665a06a9e6eb7ccceec46aeece726860737
Gabor Komuves <gabor.komuves@gmail.com>
2024-08-06 21:12:13 +0100

    am corectat metoda println

--- HelloWorld..java
+++ HelloWorld..java
@@ -2,7 +2,7 @@ public class HelloWorld{
 	
 	public static void main(String [] args){
 		
-		System.out.prinln("Hello, IT-School Java team!);
+		System.out.println("Hello, IT-School Java team!);
 	}
 	
 	
