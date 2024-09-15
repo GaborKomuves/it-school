a0f2185d9edac4520659edeabe833cccd61a40c5
Gabor Komuves <67443270+GaborKomuves@users.noreply.github.com>
2024-09-15 21:43:28 +0100

    Add files via upload
    Calase abstracte - Person

---
+++ FirstSubclass.java
@@ -0,0 +1,8 @@
+package challenge_12_Person;
+
+public class FirstSubclass extends Parent{
+    @Override
+    void message(){
+        System.out.println("Acesta este prima subclasa");
+    }
+}
---
+++ SecondSubclass.java
@@ -0,0 +1,7 @@
+package challenge_12_Person;
+
+public class SecondSubclass extends Parent{
+    void message(){
+        System.out.println("Aceasta este cea de a doua Subclasa");
+    }
+}
